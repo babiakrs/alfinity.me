a516c5aa71d938463b73f15d981840c3e5ff5eec
Бабяк Роман Сергійович <roman.babyak@valuetek.com.ua>
2024-08-12 20:46:57 +0300

    refactor: remove overlay

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React, { Suspense } from 'react';
-import { Overlay } from './components/overlay';
 import { Scene } from './components/scene';
 import { Splash } from './components/splash';
 
@@ -8,7 +7,6 @@ export function App() {
   return (
     <Suspense fallback={<Splash />}>
       <Scene />
-      <Overlay />
     </Suspense>
   );
 }
--- src/app/components/overlay/index.tsx
+++
@@ -1,5 +0,0 @@
-import styles from './overlay.module.scss';
-
-export function Overlay() {
-  return <div className={styles.overlay}></div>;
-}
