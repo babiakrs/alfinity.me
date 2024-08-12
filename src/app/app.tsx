7e3800829ec5eacb4f6dd9bfdcc7cbac1a7b6e3c
Бабяк Роман Сергійович <roman.babyak@valuetek.com.ua>
2024-08-12 20:43:00 +0300

    refactor

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,15 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Overlay } from './components/overlay';
 import { Scene } from './components/scene';
+import { Splash } from './components/splash';
 
 export function App() {
   return (
-    <>
+    <Suspense fallback={<Splash />}>
       <Scene />
       <Overlay />
-    </>
+    </Suspense>
   );
 }
-
-export default App;
