533ed4195a06fbbe36bd04063afa925f3b66aeb4
Бабяк Роман Сергійович <roman.babyak@valuetek.com.ua>
2024-08-16 15:00:11 +0300

    chore: serve app on local network

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 /// <reference types='vitest' />
+import { networkInterfaces, NetworkInterfaceInfo } from 'node:os';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+function getLocalIP(): string {
+  const localInterface = (Object.values(networkInterfaces()) as NetworkInterfaceInfo[][])
+    .flat()
+    .find(({ family, internal }) => family === 'IPv4' && !internal);
+
+  return localInterface?.address ?? 'localhost';
+}
 
 export default defineConfig({
   root: __dirname,
@@ -9,15 +18,15 @@ export default defineConfig({
 
   server: {
     port: 4200,
-    host: 'localhost',
+    host: getLocalIP()
   },
 
   preview: {
     port: 4300,
-    host: 'localhost',
+    host: 'localhost'
   },
 
-  plugins: [react(), nxViteTsPaths()],
+  plugins: [ react(), nxViteTsPaths() ],
 
   // Uncomment this if you are using workers.
   // worker: {
@@ -43,15 +52,15 @@ export default defineConfig({
     watch: false,
     globals: true,
     cache: {
-      dir: './node_modules/.vitest/alfinity.me',
+      dir: './node_modules/.vitest/alfinity.me'
     },
     environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    include: [ 'src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}' ],
 
-    reporters: ['default'],
+    reporters: [ 'default' ],
     coverage: {
       reportsDirectory: './coverage/alfinity.me',
-      provider: 'v8',
-    },
-  },
+      provider: 'v8'
+    }
+  }
 });
