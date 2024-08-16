9e55113eb4e31be40923ba96de7c26a83175bb14
Бабяк Роман Сергійович <roman.babyak@valuetek.com.ua>
2024-08-16 15:00:45 +0300

    fix: an issue when spotlight target is null

--- src/app/components/Scene/BackgroundLight.tsx
+++ src/app/components/Scene/BackgroundLight.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import { Group, Vector3 } from 'three';
+import React, { useEffect } from 'react';
+import { Group, Mesh, SpotLight, Vector3 } from 'three';
 import { useFrame, useThree } from '@react-three/fiber';
 
 export function BackgroundLight() {
   const groupRef = React.useRef<Group>(null!);
-  const planeRef = React.useRef(null!);
+  const planeRef = React.useRef<Mesh>(null!);
+  const lightRef = React.useRef<SpotLight>(null!);
   const { camera } = useThree();
 
   useFrame(() => {
     groupRef.current.position.copy(camera.position).add(camera.getWorldDirection(new Vector3()).multiplyScalar(50)).sub(new Vector3(0, 5, 0));
     groupRef.current.lookAt(camera.position);
   });
+
+  useEffect(() => {
+    if (lightRef.current && planeRef.current) {
+      lightRef.current.target = planeRef.current;
+    }
+  }, []);
 
   return (
     <group ref={groupRef}>
@@ -20,6 +27,7 @@ export function BackgroundLight() {
       </mesh>
 
       <spotLight
+        ref={lightRef}
         intensity={1000}
         position={[ 0, 0, -10 ]}
         color="#ffffff"
