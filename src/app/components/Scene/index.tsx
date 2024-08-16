5ae553ef6dc8552d6d89eab6547234688930de91
Бабяк Роман Сергійович <roman.babyak@valuetek.com.ua>
2024-08-16 15:01:02 +0300

    fix: remove suspense

--- src/app/components/Scene/index.tsx
+++ src/app/components/Scene/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { Podium } from '../Podium';
-import { BackgroundLight } from './BackgroundLight';
 import { AnimatedCamera } from './AnimatedCamera';
 import { Light } from './Light';
 import { placesConfig } from './places.config';
+import { BackgroundLight } from './BackgroundLight';
 
 export function Scene() {
   return (
-    <Canvas camera={{ position: [ 0, 70, 70 ], fov: 50 }}>
+    <Canvas camera={{ position: [ 0, 70, 70 ], fov: 50 }} style={{ position: 'absolute' }}>
       <Light />
       <BackgroundLight />
       <AnimatedCamera />
@@ -24,11 +24,7 @@ export function Scene() {
       />
 
       <Podium>
-        {placesConfig.map((place, idx) => (
-          <React.Suspense key={idx}>
-            <Podium.Place {...place} />
-          </React.Suspense>
-        ))}
+        {placesConfig.map((place, idx) => <Podium.Place key={idx} {...place} />)}
       </Podium>
     </Canvas>
   );
